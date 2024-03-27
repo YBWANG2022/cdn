a96b28536cc08f87329c449022335cfc4b294449
YBWANG2022 <113892029+YBWANG2022@users.noreply.github.com>
2024-03-27 14:47:31 +0800

    Add files via upload

---
+++ awards2024_av2.js
@@ -0,0 +1,72 @@
+$(function () {
+    Retrieve();
+});
+
+function Retrieve() {
+    var dataArray = [];
+    var URL = 'https://script.google.com/macros/s/AKfycbw07Plj5nckIqZOW4ArWgwZjW9vojCa-e-OfY1JZ_ZX06TPdM3vVKXmAIvfJX8j2HXq/exec';
+    $.ajax({
+        url: URL,
+        type: 'POST',
+        dataType: "json",
+        error: function (xhr) {
+            alert('發生錯誤！請重新再試一次～');
+        },
+        success: function (Jdata) {
+            var Info = Jdata.data;
+            for (i = 0; Info.length > i; i++) {
+                name = Info[i].name;
+                awards = Info[i].awards;
+                ctext = Info[i].ctext;
+				nominee = Info[i].nominee;
+                // 印出資料
+                print();             
+            };
+            
+            //  資料列印
+            function print(){
+                $("#table-data").append(
+                    '<tr>' +
+                        '<td class="w-10">' + nominee + '</td>' +
+                        '<td class="w-10">' + awards + '</td>' +
+                        '<td class="w-15">' + ctext + '</td>' +
+                        '<td class="w-10">' + name + '</td>' +
+                    '</tr>'
+                );
+            };
+            // 獎項搜尋            
+			$("#doaction").click(function(){
+                select();
+            });
+			
+			function select(){
+                var result = "";
+                $("#select").each(function () {
+                    result = $(this).val() ;
+                    search_table($(this).val()); 
+                });
+            };
+
+            function search_table(value){  
+                $('tbody tr').each(function(){  
+                    var found = 'false';  
+                    $(this).each(function(){  
+                         if($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0)  
+                         {  
+                              found = 'true';  
+                         }  
+                    });  
+                    if(found == 'true')  
+                    {  
+                         $(this).show();  
+                    }  
+                    else  
+                    {  
+                         $(this).hide();  
+                    }  
+               }); 
+			}  
+        }  
+    });
+};
+

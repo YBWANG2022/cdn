13165920ba4baad4b791594970007af14c796a7e
YBWANG2022 <113892029+YBWANG2022@users.noreply.github.com>
2025-02-17 10:13:24 +0800

    Add files via upload

---
+++ awards2025_P_v2.js
@@ -0,0 +1,41 @@
+function showHint(str)
+{
+var xmlhttp;
+
+        if (window.XMLHttpRequest)
+          {// code for IE7+, Firefox, Chrome, Opera, Safari
+          xmlhttp=new XMLHttpRequest();
+          }
+        else
+          {// code for IE6, IE5
+          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+          }
+        xmlhttp.onreadystatechange=function()
+          {
+                  if (xmlhttp.readyState==4 && xmlhttp.status==200)      
+                  {
+                        var result=xmlhttp.responseText;
+                        var obj = JSON.parse(result);//解析json字串為json物件形式
+                                                
+                        var html = '<table border=1 width=100%>';
+                        
+                        for (var i = 1; i < obj.length; i ++ ) {//
+                                html  += '<tr>';//
+                                for(j=0;j<obj[i].data.length;j++)
+                                {
+                                  html+= '<td>'+obj[i].data[j]+'</td>'; 
+                                }
+                                html  += '</tr>';            
+                        }
+                        html+="</table>";
+                        
+                        document.getElementById("nominee_status").innerHTML=html;
+                        if(obj.length==0) //只有一筆代表查不到資料
+                                alert('查無資料');
+                  }
+
+          }
+    var url="https://script.google.com/macros/s/AKfycbzyevoYReIOoPWHq_PeQ58BdnVaz-go6Knho27glxxc5gDIfjeSOqwR7ksFlXG60mm7/exec";
+        xmlhttp.open("get",url+"?nominee="+encodeURIComponent(str),true);
+        xmlhttp.send();
+}
